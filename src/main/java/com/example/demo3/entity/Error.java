7f1221379e44f5b04726f971b6a917fbfd8da010
khanhtrinh107 <khanhtrinh107@gmail.com>
2023-04-27 23:00:49 +0700

    init

---
+++ src/main/java/com/example/demo3/entity/Error.java
@@ -0,0 +1,12 @@
+package com.example.demo3.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Error {
+    private String message;
+}
